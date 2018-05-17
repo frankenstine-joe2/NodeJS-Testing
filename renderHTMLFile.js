12a9a87aec7c9a8926932cd2ad4f41d653de6eea
Muhammad Ansab Iqbal <ansab.iqbal@ba.com>
2018-05-17 15:15:34 +0500

    check

--- renderHTMLFile.js
+++ renderHTMLFile.js
@@ -22,6 +22,6 @@ http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         res.end();
-    });
+    }); 
 
 }).listen(8000);
